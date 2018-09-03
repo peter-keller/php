913a05ea8517ec9c9df143d234a59a43fe7b9236
peter-keller <keller.f.peter@gmail.com>
2018-09-04 00:13:46 +0100

    v1 and images

---
+++ 08_basic_website/index.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" href="/assets/styles.css">
+</head>
+
+<body id="final-example" class="bodyImage">
+    <div class="wrapper">
+        <div id="banner">
+            <a href="/" title="Return to Home">
+                <img src="img/banner.png" alt="Franklin's Fine Dining">
+            </a>
+        </div>
+        <div id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="team.php">Team</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="contact.php">Contact</a></li>
+
+            </ul>
+        </div>
+        <div class="content">
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Phone</strong>
+                    808.529.3819
+                </div>
+                <div class="column three">
+                    <strong>Location</strong>
+                    123 Kapiolani Boulevard<br>
+                    Honolulu, HI
+                </div>
+                <div class="column three last">
+                    <strong>Hours</strong>
+                    <em>Tuesday - Thursday</em><br>
+                    1:00pm - 9:00pm<br><br>
+                    <em>Friday - Saturday</em><br>
+                    4:00pm - 11:00pm<br><br>
+                    <em>Sunday - Monday</em><br>
+                    Closed<br><br>
+                </div>
+            </div>
+            <small>&copy; 2018 Peter Keller</small>
+        </div>
+    </div>
+</body>
+
+</html>
